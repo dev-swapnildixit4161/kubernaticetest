bdcf3b37a92356b7f91ab132a302f010b84757c9
dev-swapnildixit <swapnil.dixit@knoldus.com>
2023-06-28 13:11:55 +0530

    first commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+
+import org.apache.catalina.Store;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class Controller
+{
+    @GetMapping("/path")
+    public String hello()
+    {
+        return "27836t82734623";
+    }
+    
+}
